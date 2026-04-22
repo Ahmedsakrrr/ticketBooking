87fec6643a72045c9bcc4a313d40365b53f64b2f
ahmed sakr <ahmed.sakr213@gmail.com>
2026-04-22 14:41:11 +0200

    Final

--- ticketBooking/Areas/Customer/Controllers/BookController.cs
+++ ticketBooking/Areas/Customer/Controllers/BookController.cs
@@ -14,6 +14,7 @@ namespace TicketBooking.Areas.Customer.Controllers
             var books = _DbContext.Books.Include(b => b.Movie).AsQueryable();
             return View(books.AsEnumerable());
         }
+      
 
         [HttpGet]
         public IActionResult Create()
@@ -25,20 +26,48 @@ namespace TicketBooking.Areas.Customer.Controllers
         [HttpPost]
         public IActionResult Create(Book book)
         {
+           
             var movie = _DbContext.Movies.FirstOrDefault(m => m.Id == book.MovieId);
-
-            if (movie != null)
-            {
-                book.Movie = movie;           // ربط الفيلم
-                book.Movie.Price = movie.Price;
-                book.Movie.Description = movie.Description;
-                book.Movie.DateTime = movie.DateTime;
-            }
-
             _DbContext.Books.Add(book);
             _DbContext.SaveChanges();
 
             return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int id)
+        {
+            var book = _DbContext.Books.FirstOrDefault(x => x.Id == id);
+
+            ViewBag.Movies = _DbContext.Movies.ToList(); // مهم جدًا
+            return View(book);
+        }
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            var bookInDb = _DbContext.Books.AsNoTracking().FirstOrDefault(b => b.Id == book.Id);
+            if (bookInDb == null)
+            {
+                return RedirectToAction("NotFoundPage", "Home");
+            }
+            
+
+
+            _DbContext.Books.Update(book);
+            _DbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var book = _DbContext.Books.FirstOrDefault(c => c.Id == id);
+            if (book == null)
+            {
+                return RedirectToAction("NotFoundPage", "Home");
+            }
+            
+            _DbContext.Books.Remove(book);
+            _DbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
---
+++ ticketBooking/ViewModels/BookVM.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketBooking.ViewModels
+{
+    public class BookVM
+    {
+
+        public string CustomerName { get; set; }
+        public int MovieId { get; set; }
+        public List<Movie> Movies { get; set; }
+    }
+}
